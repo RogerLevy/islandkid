d9d16cb8d96bf4c094dd80054f38fe0791302840
Roger Levy <roger.levy@gmail.com>
2019-10-05 16:02:50 -0400

    directional dog animations

--- roles/boksil.f
+++ roles/boksil.f
@@ -1,12 +1,17 @@
 define-role boksil boksiling
 
+: ?animate  ( n -- )
+    dup anim# @ = if drop ;then
+    anim# !  1 32 / rate !
+;
+
 state: boksil state1
     0 0 vx 2!
-    <left> kstate if  -1 vx !  ;then
-    <right> kstate if  1 vx !  ;then
-    <up> kstate if    -1 vy !  ;then
-    <down> kstate if   1 vy !  ;then
+    <down> kstate if   1 vy !  0 ?animate ;then
+    <up> kstate if    -1 vy !  1 ?animate ;then
+    <right> kstate if  1 vx !  2 ?animate ;then
+    <left> kstate if  -1 vx !  3 ?animate ;then
 ;
 action: boksil physics  standard-physics ;
-action: boksil start  0 1 32 / animate  state1 ;
+action: boksil start  0 ?animate  state1 ;
 action: boksil hit  ." HIT!" ;
