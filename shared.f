683141f2154444c9b057b65ac86da75d55793211
Roger Levy <roger.levy@gmail.com>
2019-10-05 15:43:53 -0400

    shared.f: add NEW-TEMPLATE tool (and factors)

--- shared.f
+++ shared.f
@@ -30,3 +30,50 @@ create coldata 0 c, 0 c, #-1 c, #-1 c, 0 c, 0 c,
     w switchto
 ;
 
+
+: save-template  ( -- <name> )
+    >in @ template (?$) ?dup 0 = if
+        >in !
+        template one dup named
+    else
+        nip
+    then ( template )
+    me over copy
+    { woke off }
+;
+
+: new-actor ( -- actor )
+    stage one as 
+    16 16 sbw 2!
+    16 16 ibw 2!
+    1 1 sx 2!
+    stage scroll 2@ viewwh 2 2 2/ 2+ x 2!
+    me
+;
+
+: (script)  s" .f" strjoin ;
+
+: *script    w/o create-file throw >r  r> close-file throw ;
+
+: ?role  ( -- <name> role )
+    >in @ >r
+    role (?$) ?dup 0 = if
+        r@ >in !
+        <name> (script) file-exists not if
+            project count s[ s" roles/" +s <name> (script) +s ]s *script
+        then
+        s" add-role " s[ <name> +s bl +c <name> (script) +s ]s evaluate
+        this ( role )
+        skip
+    then
+    r> drop
+;
+
+: new-template  ( -- <name> )
+    >in @ >r
+    new-actor named
+    r@ >in ! ?role >role >!
+    r@ >in ! save-template
+    r> drop
+;
+
